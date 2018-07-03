0c7a62cc312777cc1025149aa5a0a571b4528175
Luka Krkljes <krkljes13@gmail.com>
2018-07-03 21:16:42 +0200

    added index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>testing</title>
+</head>
+<body>
+	<div>
+		<ul>
+			<li>Jedan</li>
+			<li>Dva</li>
+			<li>Tri</li>
+			<li>Cetiri</li>
+			<li>Pet</li>
+		</ul>
+	</div>
+</body>
+</html>
