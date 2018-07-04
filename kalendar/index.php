5b57fa006e56f950aa5657ee256df81b79668114
krkljes <33728717+krkljes@users.noreply.github.com>
2018-07-05 00:42:47 +0200

    Add files via upload

---
+++ kalendar/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+<link href='css/fullcalendar.min.css' rel='stylesheet'>
+<link href='css/fullcalendar.print.min.css' rel='stylesheet' media='print'>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<script src='js/moment.min.js'></script>
+<script src='js/jquery.min.js'></script>
+<script src='js/jquery-ui.min.js'></script>
+<script src='js/fullcalendar.min.js'></script>
+<script src='js/main.js'></script>
+</head>
+<body>
+  <div id='wrap'>
+
+    <!-- <div id='external-events'>
+      <h4>Draggable Events</h4>
+      <div class='fc-event'>My Event 1</div>
+      <div class='fc-event'>My Event 2</div>
+      <div class='fc-event'>My Event 3</div>
+      <div class='fc-event'>My Event 4</div>
+      <div class='fc-event'>My Event 5</div>
+      <p>
+        <input type='checkbox' id='drop-remove' />
+        <label for='drop-remove'>remove after drop</label>
+      </p>
+    </div> -->
+
+    <div id='calendar'></div>
+
+    <div style='clear:both'></div>
+
+  </div>
+</body>
+</html>
